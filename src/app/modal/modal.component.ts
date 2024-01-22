f5a28bc619db5cd62f0d3dc5f4c2dd9f271b0239
i718773 <hannah.flynn@voya.com>
2024-01-22 16:46:29 -0500

    focus example

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -7,8 +7,15 @@ import { ModalController } from '@ionic/angular';
   styleUrls: []
 })
 export class ModalComponent {
+  @ViewChild('ionButton', { read: ElementRef, static: true })
+  ionButton: ElementRef;
   constructor(private modal: ModalController) {}
   closeClicked() {
     this.modal.dismiss();
+  }
+
+  ngAfterViewInit() {
+    console.log(this.ionButton);
+    this.ionButton.nativeElement.focus();
   }
 }
